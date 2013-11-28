e7933cba678f45e4b8fd9be923efc2c0e20411cd
vagrant <vagrant@precise32.(none)>
2013-11-28 06:34:21 +0000

    a simple webserver

---
+++ webserver.go
@@ -0,0 +1,35 @@
+package main
+import (
+	"io/ioutil"
+	"net/http"
+	"fmt"
+)
+type Page struct {
+    Title string
+    Body  []byte
+}
+
+func (p *Page) save() error {
+    filename := p.Title + ".txt"
+    return ioutil.WriteFile(filename, p.Body, 0600)
+}
+
+func loadPage(title string) (*Page, error) {
+    filename := title + ".txt"
+    body, err := ioutil.ReadFile(filename)
+    if err != nil {
+        return nil, err
+    }
+    return &Page{Title: title, Body: body}, nil
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+    title := r.URL.Path[len("/view/"):]
+    p, _ := loadPage(title)
+    fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+}
+func main() {
+    http.HandleFunc("/view/", viewHandler)
+    http.ListenAndServe(":8080", nil)
+}
+
